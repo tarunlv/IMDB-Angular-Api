814a68f4ff25150e1b97547cf3305308c72bb24a
Tarun Verma <tarunverma0098@gmail.com>
2023-11-27 11:10:48 +0530

    upgraded angular v10 to v11

--- src/app/components/genre-container/genre-container.component.spec.ts
+++ src/app/components/genre-container/genre-container.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { GenreContainerComponent } from './genre-container.component';
 
@@ -6,7 +6,7 @@ describe('GenreContainerComponent', () => {
   let component: GenreContainerComponent;
   let fixture: ComponentFixture<GenreContainerComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ GenreContainerComponent ]
     })
--- src/app/components/home-screen/home-screen.component.spec.ts
+++ src/app/components/home-screen/home-screen.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { HomeScreenComponent } from './home-screen.component';
 
@@ -6,7 +6,7 @@ describe('HomeScreenComponent', () => {
   let component: HomeScreenComponent;
   let fixture: ComponentFixture<HomeScreenComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ HomeScreenComponent ]
     })
